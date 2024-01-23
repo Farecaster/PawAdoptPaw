04f726c0d4df284c00c29b7e856a9fb7c13d7a85
dindo <diag.devillena.up@phinmaed.com>
2024-01-23 15:52:37 +0800

    qweqwe

---
+++ laravel/resources/views/login.blade.php
@@ -0,0 +1,42 @@
+
+@extends('layout.app');
+
+@section('content');
+<section id="login" class="login section-padding">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-header bg-success text-white">
+                        <h3 class="text-center">Sign Up</h3>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control" id="username" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="email" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="password" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="confirmPassword" class="form-label">Confirm Password</label>
+                                <input type="password" class="form-control" id="confirmPassword" required>
+                            </div>
+                            <button type="submit" class="btn btn-success">Sign Up</button>
+                        </form>
+                        <p class="mt-3" style="text-align: center;">Already have an account? <a
+                                href="{{ route('login') }}">Log In</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
---
+++ laravel/resources/views/shared/footer.blade.php
@@ -0,0 +1,52 @@
+<footer>
+    <footer class="footer bg-dark text-light py-5 bottom mt-5">
+        <div class="container">
+            <div class="row justify-content-center gy-3">
+                <div class="col-9 col-lg-6 paw-adopt-paw">
+                    <p class="display-5 text-warning text-center text-lg-start">
+                        PAW ADOPT PAW
+                    </p>
+                    <small class="text-white-50">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Perspiciatis a cumque recusandae impedit magnam provident fugiat,
+                        sequi eligendi. Sapiente impedit nemo obcaecati quo minima officia
+                        quasi unde soluta sint numquam.</small>
+                </div>
+                <div class="col-9 col-lg-6">
+                    <p class="display-5 text-warning text-center text-lg-start">
+                        P.A.W
+                    </p>
+                    <small class="text-white-50">P.A.W stands for all pets are amazing Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit.</small>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-5">
+            <div class="row justify-content-center justify-content-lg-start">
+                <div class="d-none d-lg-inline col-9 col-lg-6">
+                    <p class="text-white">
+                        Copyright 2024 all rights reserved by:
+                        <span class="text-warning">P.A.W</span>
+                    </p>
+                </div>
+                <div class="d-lg-none col-sm-6 text-center">
+                    <p class="text-white text-size">
+                        Copyright 2024 all rights reserved by:
+                        <span class="text-warning">P.A.W</span>
+                    </p>
+                </div>
+                <div class="col-9 col-lg-6 text-center">
+                    <p>
+                        <a href="" class="no-text-decoration text-light">
+                            <i class="bi bi-instagram"></i></a>
+                        <a href="" class="no-text-decoration text-light ms-4">
+                            <i class="bi bi-facebook"></i></a>
+                        <a href="" class="no-text-decoration text-light ms-4">
+                            <i class="bi bi-twitter-x"></i></a>
+                        <a href="" class="no-text-decoration text-light ms-4">
+                            <i class="bi bi-envelope-paper-heart"></i></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</footer>
