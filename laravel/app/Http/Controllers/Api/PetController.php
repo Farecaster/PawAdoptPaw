19c81cb3777425a1019874a75a64706010e76443
paul <sumalinog1124@gmail.com>
2024-02-12 00:07:01 +0800

    update ulit

--- laravel/app/Http/Controllers/Api/PetController.php
+++ laravel/app/Http/Controllers/Api/PetController.php
@@ -8,16 +8,16 @@ use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
-    public function index(){
-        $pets = Pet::query();
-        
-        $pets = $pets->whereDoesntHave('adoptionRequests', function ($query) {
-            $query->whereIn('status', ['accepted', 'done'])->where('user_id', auth()->user()->id);
-        })->get();
+    public function index()
+    {
+        $currentUser = auth()->user(); // Assuming you are using Laravel's built-in authentication
+
+        $pets = Pet::whereDoesntHave('adoptionRequests', function ($query) {
+            $query->whereIn('status', ['accepted', 'done']);
+        })
+            ->where('user_id', '!=', $currentUser->id)
+            ->get();
 
         return response()->json($pets);
-
     }
-
-    
 }
