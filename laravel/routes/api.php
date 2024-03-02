ee1f9a2b27f7cf79383fb706049f16359cb3d7a7
paul <sumalinog1124@gmail.com>
2024-03-02 22:55:24 +0800

    lol

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\AdoptionRequestController;
+use App\Http\Controllers\Api\AdoptionRequestController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PetController;
 use App\Http\Controllers\Api\UserController;
@@ -10,14 +10,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-
-    
 });
 
 
 
 //PRIVATE ROUTES
-Route::group(['middleware' => ['auth:sanctum','not_banned']], function () {
+Route::group(['middleware' => ['auth:sanctum', 'not_banned']], function () {
 
 
     //AUTH CONTROLLER
@@ -38,15 +36,15 @@ Route::group(['middleware' => ['auth:sanctum','not_banned']], function () {
 
 
 //AR CONTROLLER
-Route::group(['middleware' => ['auth:sanctum','not_banned']], function () {
+Route::group(['middleware' => ['auth:sanctum', 'not_banned']], function () {
     Route::get('/pets/{pet}/adopt', [AdoptionRequestController::class, 'create']);
     Route::post('/pets/{pet}/adopt', [AdoptionRequestController::class, 'store']);
-    
+
     Route::get('/my-requests', [AdoptionRequestController::class, 'myRequest']);
     Route::get('/my-requests/{id}/edit', [AdoptionRequestController::class, 'myRequestEdit']);
     Route::put('/my-requests/{id}', [AdoptionRequestController::class, 'update']);
     Route::delete('/my-requests/{id}', [AdoptionRequestController::class, 'myRequestDestroy']);
-    
+
     Route::get('/incoming-requests', [AdoptionRequestController::class, 'incomingRequest']);
     Route::put('/incoming-requests/{id}/accept', [AdoptionRequestController::class, 'acceptRequest']);
     Route::put('/incoming-requests/{id}/reject', [AdoptionRequestController::class, 'rejectRequest']);
@@ -59,13 +57,11 @@ Route::group(['middleware' => ['auth:sanctum','not_banned']], function () {
     Route::get('/on-going-requests/details/{id}', [AdoptionRequestController::class, 'onGoingRequestDetails']);
     //history
     Route::get('/history', [AdoptionRequestController::class, 'history']);
-    
-    
+
+
     Route::get('/users/current', [UserController::class, 'getCurrentUserId']);
     Route::get('/users/{id}', [UserController::class, 'show'])->name('user.profile');
     Route::put('/users/{id}', [UserController::class, 'update'])->name('user.update');
-
-
 });
 
 
